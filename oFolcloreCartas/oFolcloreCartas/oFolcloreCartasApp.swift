74b5a34ce832fdeddd45d8c219fd1cfb2bb4673a
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-05-20 20:15:19 -0300

    Subindo Projeto

---
+++ oFolcloreCartas/oFolcloreCartas/oFolcloreCartasApp.swift
@@ -0,0 +1,17 @@
+//
+//  oFolcloreCartasApp.swift
+//  oFolcloreCartas
+//
+//  Created by Higor  Lo Castro on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct oFolcloreCartasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
